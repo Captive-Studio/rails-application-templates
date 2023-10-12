39a18fab15e5138037145c34a20cf23821ca82b8
Clément Prod'homme <clement.prod-homme@captive.fr>
2023-10-12 10:36:54 +0200

    ✨ Ajoute le template database_uuid

---
+++ database_uuid.rb
@@ -0,0 +1,20 @@
+# Add the "pg" gem to your Gemfile for PostgreSQL support
+gem 'pg'
+
+# Generate a migration to enable the "pgcrypto" extension for UUIDs
+generate 'migration', 'EnablePgcryptoExtension', 'enable_pgcrypto_extension'
+
+# In the generated migration file, add the SQL command to enable the extension
+# (You may need to adjust this based on your specific PostgreSQL version)
+insert_into_file 'db/migrate/XXXXXXXX_enable_pgcrypto_extension.rb', after: "def change\n" do
+  <<~RUBY
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+  RUBY
+end
+
+# Run the migration to enable the extension
+rails_command 'db:migrate'
+
+# Initialize the database with UUIDs as the default data type
+rails_command 'db:setup'
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+# rails-application-templates
+
+## Usage
+
+| name | description  | command  |
+|------|--------------|----------|
+| database_uuid | Active l'extension UUID sur la base de donnée (PostgreSQL) afin de remplacer les ID (bigint) en UUID | `rails app:template LOCATION=https://github.com/Captive-Studio/rails-application-templates` |
+
+## Rules
+
+- chaque template doit être le plus unitaire possible
+- les templates doivent être écris en `snake_case`
