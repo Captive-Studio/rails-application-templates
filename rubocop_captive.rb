2686f59716647bb12a57ed4d9d60fb35217fb789
Clément Prod'homme <clement.prod-homme@captive.fr>
2023-11-15 10:59:21 +0100

    🐛 Corrige le script rubocop_captive

--- rubocop_captive.rb
+++ rubocop_captive.rb
@@ -8,27 +8,29 @@ gem_group :development, :test do
   end
 end
 
-rubocop_content = <<~EOF
-inherit_gem:
-  rubocop-config-captive:
-    - config/default.yml
-EOF
+after_bundle do
+  rubocop_content = <<~EOF
+  inherit_gem:
+    rubocop-config-captive:
+      - config/default.yml
+  EOF
 
-file ".rubocop.yml", rubocop_content
+  file ".rubocop.yml", rubocop_content
 
-correction_erreur = no?("Souhaitez-vous lancer la correction d'erreur ? [Y/n]", :yellow)
+  correction_erreur = no?("Souhaitez-vous lancer la correction d'erreur ? [Y/n]")
 
-if correction_erreur
-  run "bundle exec rubocop -A"
-  run "gem install bundle-locker"
-  run "bundle-locker ./Gemfile"
+  if correction_erreur
+    run "bundle exec rubocop -A"
+    run "gem install bundle-locker"
+    run "bundle-locker ./Gemfile"
 
-  # Manually edit Gemfile and adjust constraints "x.x.x", "~> x.x.x", etc
-  file_path = "Gemfile"
-  # Lis le contenu du fichier Gemfile
-  gemfile_content = File.read(file_path)
-  # Remplace "x.x.x" par "~> x.x.x"
-  modified_content = gemfile_content.gsub(/"(\d+\.\d+\.\d+)"/, '"~> \1"')
-  # Écris le contenu modifié dans le fichier Gemfile
-  File.open(file_path, "w") { |file| file.puts modified_content }
+    # Manually edit Gemfile and adjust constraints "x.x.x", "~> x.x.x", etc
+    file_path = "Gemfile"
+    # Lis le contenu du fichier Gemfile
+    gemfile_content = File.read(file_path)
+    # Remplace "x.x.x" par "~> x.x.x"
+    modified_content = gemfile_content.gsub(/"(\d+\.\d+\.\d+)"/, '"~> \1"')
+    # Écris le contenu modifié dans le fichier Gemfile
+    File.open(file_path, "w") { |file| file.puts modified_content }
+  end
 end
