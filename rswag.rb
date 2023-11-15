2d04dadb8a59094d73d5207aa4f36dfa2ab5a207
Clément Prod'homme <clement.prod-homme@captive.fr>
2023-11-15 14:23:47 +0100

    ⚗️ Améliore le template rswag

--- rswag.rb
+++ rswag.rb
@@ -54,3 +54,23 @@ after_bundle do
     RUBY
   end
 end
+
+git add: "."
+git commit: "➕ Ajoute la gem rswag"
+
+run "yarn add husky -D"
+run "yarn add lint-staged -D"
+run "npx husky add .husky/pre-commit \"npx lint-staged\""
+run "git add .husky/pre-commit"
+
+inject_into_file "package.json", after: "\"lint-staged\": {\n" do
+  <<~EOF
+    "spec/requests/**/*_spec.rb": [
+      "SWAGGER_DRY_RUN=0 rails rswag",
+      "git add swagger/**/*"
+    ]
+  EOF
+end
+
+git add: "."
+git commit: "🔧 Configure le pre-commit de documentation"
